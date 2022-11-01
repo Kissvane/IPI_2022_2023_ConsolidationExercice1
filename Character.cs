8f70c7aa93d05bab09902d2bbc937badadcad6ed
Kissvane <adrien@nebulae.world>
2022-11-01 17:29:43 +0100

    Exercice1

---
+++ Character.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorrectionConsolidation
+{
+    public enum CharacterClass { ARCHER = 0, WARRIOR = 1, HERO = 2, WIZARD = 3 };
+
+
+    public class Character
+    {
+        public string Name { get; set; }
+        public int Strength { get; set; }
+        public int Speed { get; set; }
+        public int Wisdom { get; set; }
+        public int Charisma { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} {Strength} {Speed} {Wisdom} {Charisma}";
+        }
+    }
+
+    public class ClassedCharacter : Character
+    {
+        public CharacterClass Class;
+
+        public ClassedCharacter(Character character)
+        {
+            this.Name = character.Name;
+            this.Speed = character.Speed;
+            this.Strength = character.Strength;
+            this.Wisdom = character.Wisdom;
+            this.Charisma = character.Charisma;
+
+            int max = CalculateMaximumStat();
+            if (Speed == max)
+                Class = CharacterClass.ARCHER;
+            else if (Wisdom == max)
+                Class = CharacterClass.WIZARD;
+            else if (Strength == max)
+                Class = CharacterClass.WARRIOR;
+            else
+                Class = CharacterClass.HERO;
+        }
+
+        public override string ToString()
+        {
+            return string.Concat($" {Class} ",base.ToString());
+        }
+
+        public int CalculateMaximumStat()
+        {
+            return Math.Max(Math.Max(Math.Max(Strength, Speed), Charisma), Wisdom);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace CorrectionConsolidation
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string directoryPath = Path.Combine(Directory.GetCurrentDirectory(), "Save");
+            string directoryPath2 = Path.Combine(Directory.GetCurrentDirectory(), "Save2");
+            Stopwatch stopwatch = new Stopwatch();
+            //synchronous mehods
+            //is here only to compare speed
+            stopwatch.Start();
+            List<ClassedCharacter> characters = DeserializeAll(directoryPath);
+            File.WriteAllText(Path.Combine(directoryPath2,"SynchronSave.json"),JsonConvert.SerializeObject(characters));
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+            stopwatch.Reset();
+            //asyncronous mehod start here
+            stopwatch.Start();
+            Task<List<ClassedCharacter>> tasks = Deserialize(directoryPath);
+            await tasks;
+            List<ClassedCharacter> characters2 = tasks.Result;
+            File.WriteAllText(Path.Combine(directoryPath2, "AsynchronSave.json"), JsonConvert.SerializeObject(characters2));
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+        }
+
+        static List<ClassedCharacter> DeserializeAll(string directoryPath)
+        {
+            List<ClassedCharacter> result = new List<ClassedCharacter>();
+            string[] filenames = Directory.GetFiles(directoryPath, "*", SearchOption.TopDirectoryOnly);
+            int fCount = filenames.Length;
+            List<Task<Character>> tasks = new List<Task<Character>>();
+            foreach (string name in filenames)
+            {
+                string path = Path.Combine(directoryPath, name);
+                Character c = DeserializeCharacter(path);
+                result.Add(new ClassedCharacter(c));
+            }
+
+            return result;
+        }
+
+        static async Task<List<ClassedCharacter>> Deserialize(string directoryPath)
+        {
+            List<ClassedCharacter> result = new List<ClassedCharacter>();
+            string[] filenames = Directory.GetFiles(directoryPath, "*", SearchOption.TopDirectoryOnly);
+            int fCount = filenames.Length;
+            List<Task<Character>> tasks = new List<Task<Character>>();
+            //load
+            foreach(string name in filenames)
+            {
+                string path = Path.Combine(directoryPath, name);
+                Task<Character> t = Task.Run(() => DeserializeCharacter(path));
+                tasks.Add(t);
+            }
+
+            await Task.WhenAll<Character>(tasks);
+            foreach (Task<Character> task in tasks)
+            {
+                result.Add(new ClassedCharacter(task.Result));
+            }
+
+            return result;
+        }
+
+        static Character DeserializeCharacter(string filePath)
+        {
+            string data =File.ReadAllText(filePath);
+            Character result = JsonConvert.DeserializeObject<Character>(data);
+            return result;
+        }
+    }
+}
